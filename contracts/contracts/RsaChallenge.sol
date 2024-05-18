3b928fd3f95a98ab88a7787483381b2d82425fac
yellow <8539006+yellowBirdy@users.noreply.github.com>
2024-05-18 22:12:04 +0200

    wip

--- contracts/contracts/RsaChallenge.sol
+++ contracts/contracts/RsaChallenge.sol
@@ -9,37 +9,33 @@ contract RsaChallenge {
     address payable public owner;
     Groth16Verifier private verifier;
 
-    mapping(uint256 => uint256) public challenges;
-    mapping(uint256 => uint256) private challengesSet;
-    mapping(uint256 => address) public challengesCompleted;
+    uint256[5][] public liveness;
 
-    uint256 public challengesCount;
+    uint256 public round;
 
-    event Verified(address user, uint256 challenge);
+    event Verified(uint256 round);
 
     constructor(address snarkVerifier) payable {
         verifier = Groth16Verifier(snarkVerifier);
         owner = payable(msg.sender);
-        challengesCount = 0;
+        liveness[0][0] = 100;
+        liveness[0][1] = 100;
+        liveness[0][2] = 100;
+        liveness[0][3] = 100;
+        liveness[0][4] = 100;
+
     }
 
-    function newChallenge(uint256 challenge) public {
-        require(msg.sender == owner, "You aren't the owner");
-        require(challengesSet[challenge] == 0, "Challenge already exists");
-        challengesSet[challenge] = 1;
-        challenges[challengesCount] = challenge;
-        challengesCount++;
-    }
-
-    function solveChallenge(uint256 challenge, uint256[8] memory proof) public {
-        require(challengesSet[challenge] == 1, "Challenge does not exist");
-        require(challengesCompleted[challenge] == address(0), "Challenge already solved");
+    function processRound(uint256[8] memory proof, uint256[15] memory _pubSignals) public {
+        uint256[5] storage lastRound = liveness[round];
+        //TODO: game finished?
+        // require(assertion, "Game already concluded");
 
         uint256[2] memory pA = [proof[0], proof[1]];
         uint256[2][2] memory pB = [[proof[2], proof[3]], [proof[4], proof[5]]];
         uint256[2] memory pC = [proof[6], proof[7]];
 
-        require(verifier.verifyProof(pA, pB, pC, [challenge]), "Invalid proof");
+        require(verifier.verifyProof(pA, pB, pC, _pubSignals), "Invalid proof");
 
         challengesCompleted[challenge] = msg.sender;
         emit Verified(msg.sender, challenge);
